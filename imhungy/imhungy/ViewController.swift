83f4a25deb7cdb0beece01f1373e4cf8be471264
sj123r <sammyjangsehee@gmail.com>
2022-10-09 23:19:10 +0900

    first commit

---
+++ imhungy/imhungy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imhungy
+//
+//  Created by Sehee Jang on 2022/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ imhungy/imhungyUITests/imhungyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  imhungyUITestsLaunchTests.swift
+//  imhungyUITests
+//
+//  Created by Sehee Jang on 2022/10/09.
+//
+
+import XCTest
+
+class imhungyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
